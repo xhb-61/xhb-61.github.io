ba53187da5280a8b53695693abf9f6fb44d0794c
Haobo Xu <btsyyxhb@126.com>
2026-05-18 20:57:19 +0800

    Move publications before experience

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'experience', 'publications', 'awards'];
+const section_names = ['home', 'publications', 'experience', 'awards'];
 
 
 window.addEventListener('DOMContentLoaded', event => {
